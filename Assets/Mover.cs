99e088068d9bc3c709bb009da7ee4fbe333c31ee
NobSwitch <shapternathan@protonmail.com>
2022-05-10 12:13:08 -0400

    playing around

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float xValue = 0.0f;
+    [SerializeField] float yValue = 0.01f;
+    [SerializeField] float zValue = 0.0f;
     void Start()
     {
         
@@ -13,6 +15,6 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(xValue,yValue,zValue);
     }
 }
